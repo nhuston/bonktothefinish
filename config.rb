e12ee6d0fc5b2e01a8388b75c6e96cfc4d9f6471
james <jtronlabs@gmail.com>
2017-05-09 11:54:16 -0400

    added sitemap.xml

--- config.rb
+++ config.rb
@@ -2,6 +2,7 @@
 # Page options, layouts, aliases and proxies
 ###
 require 'html-proofer'
+require 'builder'
 
 # Per-page layout changes:
 #
@@ -9,6 +10,7 @@ require 'html-proofer'
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+page "/sitemap.xml", :layout => false
 
 set :markdown_engine, :kramdown
 set :markdown, parse_block_html: true
