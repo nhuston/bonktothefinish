6fc4bb5c7ea33a8adc19aea2ba9d3407fa817439
James <jtronlabs@gmail.com>
2018-06-24 23:18:17 -0700

    multiple slides work on same page

--- source/javascripts/slideshow.js
+++ source/javascripts/slideshow.js
@@ -1,38 +1,42 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
 var slideIndices = {};
 
-function initSlide(id){
-  console.log("ASD");
-  slideIndices[id] = 1;
-  
+function initSlide(slide_id){
+  slideIndices[slide_id] = 0;
+  showSlides(slide_id);
 }
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function addToSlideIndex(slide_id, n) {
+  slideIndices[slide_id] += n;
+  showSlides(slide_id);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function setCurrentSlide(slide_id, n) {
+  slideIndices[slide_id] = n;
+  showSlides(slide_id);
 }
 
-function showSlides(n) {
+function showSlides(slide_id) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+
+  var slideContainer = document.getElementById(slide_id);
+  var slides = slideContainer.getElementsByClassName("mySlides");
+  var dots = slideContainer.getElementsByClassName("demo");
+  var captionText = slideContainer.getElementsByClassName("caption")[0];
+
+  var slideIndex = slideIndices[slide_id];
+  if (slideIndex >= slides.length) {slideIndex = 0; }
+  if (slideIndex < 0) {slideIndex = slides.length-1; }
+  slideIndices[slide_id] = slideIndex;
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  slides[slideIndex].style.display = "block";
+  dots[slideIndex].className += " active";
+  captionText.innerHTML = dots[slideIndex].alt;
 }
