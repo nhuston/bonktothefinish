d37f2b2cdf465067024bf0453f4fab029a918d5e
James <jtronlabs@gmail.com>
2019-04-03 20:19:56 -0700

    shrink img

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 git pull origin master
 
 #netflify shrinks imgs, but does not convert to diff file types. Keep image shrinking
-sh source/images/shrink_img.sh $(pwd)/source/images/blog
-# python3 source/images/shrink_imgs.py
+# sh source/images/shrink_img.sh $(pwd)/source/images/blog
+python3 source/images/shrink_imgs.py
 
 git add -A
 git commit -m "$1"
